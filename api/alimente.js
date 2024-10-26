8f093fc11159847da4360aa39cf8405b409d1d07
Alin Grigorescu <alin.grig87@gmail.com>
2024-10-26 15:05:53 +0300

    feature: GET POST PUT DELETE operations for admin page

--- api/alimente.js
+++ api/alimente.js
@@ -6,3 +6,41 @@ export async function getAllProducts() {
 
 	return products;
 }
+
+export async function getProductById(id) {
+	const response = await fetch(`${URL}/${id}`);
+	const product = await response.json();
+
+	return product;
+}
+
+export async function addNewProduct(product) {
+	const response = await fetch(URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(product),
+	});
+
+	const newProduct = await response.json();
+	return newProduct;
+}
+
+export async function updateProduct(product, id) {
+	const response = await fetch(`${URL}/${id}`, {
+		method: 'PUT',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(product),
+	});
+
+	const editedProduct = await response.json();
+
+	return editedProduct;
+}
+
+export async function deleteProduct(id) {
+	await fetch(`${URL}/${id}`, { method: 'DELETE' });
+}
