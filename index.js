2869c8b75ae3df7b40654a03781c4dc392cc868b
Alin Grigorescu <alin.grig87@gmail.com>
2024-10-28 19:41:21 +0200

    add: save cart to local storage

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ async function displayProducts() {
 	document.querySelector('.products').innerHTML = products
 		.map(mapProductToCard)
 		.join('');
+
+	const addToCartButtons = document.querySelectorAll('.add-to-cart');
+	addToCartButtons.forEach((button) => {
+		button.addEventListener('click', () => {
+			const productId = button.getAttribute('data-productId');
+			let cart = JSON.parse(localStorage.getItem('cart')) || {};
+
+			if (cart[productId]) {
+				cart[productId].quantity++;
+			} else {
+				cart[productId] = { quantity: 1 };
+			}
+
+			localStorage.setItem('cart', JSON.stringify(cart));
+		});
+	});
 }
